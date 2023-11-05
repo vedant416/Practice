a578ffc574fb148ec031da4c39bbc58adc7faccb
__Vedant__ <72561951+vedant416@users.noreply.github.com>
2023-11-05 20:49:06 +0530

    Time: 108 ms (30.66%), Space: 22.2 MB (20.34%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # key() : value(list)
+        hm = collections.defaultdict(list)
+        
+        for string in strs:
+            countArray = [0] * 26
+            
+            # build key
+            for char in string:
+                index = ord(char) - ord("a")
+                countArray[index] += 1 
+            key = tuple(countArray)
+            
+            # add string using key 
+            hm[key].append(string)
+        return hm.values()
