f9fa57b9d46b334e646a4f1ae6e5654cf4b68d84
__Vedant__ <72561951+vedant416@users.noreply.github.com>
2023-11-07 00:09:13 +0530

    Time: 42 ms (34.19%), Space: 16.6 MB (7.60%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        def dfs(r, c):
+            if r >= m or c >= n:
+                return 0
+            if r == m - 1 and c == n - 1:
+                return 1
+            return dfs(r+1, c) + dfs(r, c+1)
+            
+        return dfs(0, 0)
