ca7d7cbc740fb3e426546998db3f716ae7ed3f14
__Vedant__ <72561951+vedant416@users.noreply.github.com>
2023-11-07 23:21:35 +0530

    Time: 43 ms (27.45%), Space: 16.4 MB (12.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,11 +1,17 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
+        memo = [[-1] * n for _ in range(m)]        
+    
         def dfs(r, c):
             if r >= m or c >= n:
                 return 0
             if r == m - 1 and c == n - 1:
                 return 1
-            return dfs(r+1, c) + dfs(r, c+1)
+            if memo[r][c] != -1:
+                return memo[r][c]
+            
+            result = dfs(r+1, c) + dfs(r, c+1)
+            memo[r][c] = result
+            return result
             
         return dfs(0, 0)
