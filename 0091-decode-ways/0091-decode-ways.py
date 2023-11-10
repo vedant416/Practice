b65217f6e35eb0c37364fbe540eddf4700ae002e
__Vedant__ <72561951+vedant416@users.noreply.github.com>
2023-11-11 00:16:31 +0530

    Time: 47 ms (14.30%), Space: 16.5 MB (25.07%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,20 +1,20 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        @cache
-        def dfs(i):
-            n = len(s)
-            if i > n:
-                return 0
-            if i == n:
+        memo = {}
+
+        def decode(index):
+            if index == len(s):
                 return 1
-            if s[i] == "0":
+            if s[index] == '0':
                 return 0
-            x1 = dfs(i+1)
-            x2 = 0
-            
-            if i+1 < len(s) and int(s[i] + s[i+1]) < 27:
-                x2 = dfs(i+2)
-            
-            x = x1 + x2
-            return x
-        return dfs(0)
+            if index in memo:
+                return memo[index]
+
+            result = decode(index + 1)
+            if index+1 < len(s) and int(s[index] + s[index+1]) < 27:
+                result += decode(index + 2)
+
+            memo[index] = result
+            return result
+
+        return decode(0)
