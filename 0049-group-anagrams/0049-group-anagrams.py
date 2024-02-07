7a6921cd22982f69ae6d57c4721bba4fdc215c25
__Vedant__ <72561951+vedant416@users.noreply.github.com>
2024-02-07 16:10:22 +0530

    Time: 93 ms (53.74%), Space: 22.3 MB (20.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,25 +1,25 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hm = collections.defaultdict(list)
-        for s in strs:
-            key = ''.join(sorted(s))
-            hm[key].append(s)
-            
-        return hm.values()
 # class Solution:
 #     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         # key() : value(list)
 #         hm = collections.defaultdict(list)
-        
-#         for string in strs:
-#             countArray = [0] * 26
+#         for s in strs:
+#             key = ''.join(sorted(s))
+#             hm[key].append(s)
             
-#             # build key
-#             for char in string:
-#                 index = ord(char) - ord("a")
-#                 countArray[index] += 1 
-#             key = tuple(countArray)
-            
-#             # add string using key 
-#             hm[key].append(string)
 #         return hm.values()
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # key() : value(list)
+        hm = collections.defaultdict(list)
+        
+        for string in strs:
+            countArray = [0] * 26
+            
+            # build key
+            for char in string:
+                index = ord(char) - ord("a")
+                countArray[index] += 1 
+            key = tuple(countArray)
+            
+            # add string using key 
+            hm[key].append(string)
+        return hm.values()
